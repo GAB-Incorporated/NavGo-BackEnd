e1932eea6bdb4c9733c214aa9c3d7f2aa05fd8d7
brunofsilva2 <bruno.19fs@gmail.com>
2024-08-21 23:31:44 -0300

    chore: update database - add modules table and reference it in related tables

--- createDatabase.sql
+++ createDatabase.sql
@@ -1,27 +1,22 @@
+create database if not exists navgo_db;
+
 use navgo_db;
 
-create table buildings(
+create table if not exists buildings(
     building_id int auto_increment primary key,
     building_name varchar(50),
     description varchar(100),
     soft_delete bool default false
 );
 
-create table location_types(
+create table if not exists location_types(
     type_id int auto_increment primary key,
     type_name varchar(50),
     description varchar(100),
     soft_delete bool default false
 );
 
-create table subjects(
-    subject_id int auto_increment primary key,
-    subject_name varchar(100),
-    module int,
-    soft_delete bool default false
-);
-
-create table locations(
+create table if not exists locations(
     location_id int auto_increment primary key,
     campus varchar(100),
     building_id int,
@@ -36,21 +31,25 @@ create table locations(
     foreign key (location_type_id) references location_types(type_id)
 );
 
-create table users(
+create table if not exists users(
     user_id int auto_increment primary key,
     first_name varchar(50),
     last_name varchar(150),
     nick_name varchar(50),
     email varchar(200) unique,
     password_hash varchar(255),
-    is_student bool default true,
-    is_teacher bool default false,
-    is_coordinator bool default false,
+    user_type varchar(50),
     photo_id varchar(255),
     soft_delete bool default false
 );
 
-create table classes(
+create table if not exists subjects(
+    subject_id int auto_increment primary key,
+    subject_name varchar(100),
+    soft_delete bool default false
+);
+
+create table if not exists classes(
     class_id int auto_increment primary key,
     subject_id int,
     start_hour time,
@@ -62,7 +61,7 @@ create table classes(
     foreign key (subject_id) references subjects(subject_id)
 );
 
-create table periods(
+create table if not exists periods(
     period_id int auto_increment primary key,
     start_hour time,
     end_hour time,
@@ -70,16 +69,24 @@ create table periods(
     soft_delete bool default false
 );
 
-create table courses(
+create table if not exists courses(
     course_id int auto_increment primary key,
     course_name varchar(100),
-    module_qnt int,
     coordinator_id int,
     soft_delete bool default false,
     foreign key (coordinator_id) references users(user_id)
 );
 
-create table course_periods(
+create table if not exists modules(
+    module_id int auto_increment primary key,
+    module_number int,
+    course_id int,
+    module_name varchar(5),
+    soft_delete bool default false,
+    foreign key (course_id) references courses(course_id)
+);
+
+create table if not exists course_periods(
     course_id int,
     period_id int,
     soft_delete bool default false,
@@ -88,17 +95,14 @@ create table course_periods(
     foreign key (period_id) references periods(period_id)
 );
 
-create table students(
+create table if not exists students(
     student_id int auto_increment primary key,
     user_id int,
-    course_id int,
-    module int,
     soft_delete bool default false,
-    foreign key (user_id) references users(user_id),
-    foreign key (course_id) references courses(course_id)
+    foreign key (user_id) references users(user_id)
 );
 
-create table class_info(
+create table if not exists class_info(
     relation_id int auto_increment primary key,
     course_id int,
     class_id int,
@@ -109,11 +113,21 @@ create table class_info(
     foreign key (location_id) references locations(location_id)
 );
 
-create table student_info(
+create table if not exists student_info(
     student_id int,
     relation_id int,
+	module_id int,
     soft_delete bool default false,
     primary key (student_id, relation_id),
     foreign key (student_id) references students(student_id),
-    foreign key (relation_id) references class_info(relation_id)
+    foreign key (relation_id) references class_info(relation_id),
+	foreign key (module_id) references modules(module_id)
+
+);
+
+create table if not exists verification_codes(
+    id int auto_increment primary key,
+    code varchar(10) not null,
+    user_id int not null,
+    foreign key (user_id) references users(user_id)
 );

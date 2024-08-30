8713882db05ce2643a0429ef4af76bba7b74dc86
Guilherme Alves <drdrakino@gmail.com>
2024-08-30 16:34:19 -0300

    fix: Ajustar Json recebido e query sql de matérias

--- src/controllers/subjectsController.js
+++ src/controllers/subjectsController.js
@@ -4,9 +4,9 @@ import service from '../services/subjectsService.js';
 const routes = express.Router();
 
 routes.post('/', async (request,response) => {
-    const {subject_name,module} = request.body;
+    const {subject_name} = request.body;
 
-    await service.createSubjects(subject_name,module);
+    await service.createSubjects(subject_name);
 
     return response.status(201).send({
         message: 'Materia cadastrada com sucesso!'
@@ -24,9 +24,9 @@ routes.delete('/:idSubject', async (request, response) =>{
 
 routes.put('/:id', async (request, response) => {
     const { id } = request.params;
-    const { subject_name, module } = request.body;
+    const { subject_name } = request.body;
 
-    await service.updateSubject(id, subject_name, module);
+    await service.updateSubject(id, subject_name );
 
     return response.status(200).send({
         message: 'Materia atualizada com sucesso!'
--- src/services/subjectsService.js
+++ src/services/subjectsService.js
@@ -2,7 +2,7 @@ import database from '../repository/mySQL.js';
 
 async function createSubjects(subject_name, module){
 
-    const typeSubjects = "INSERT INTO subjects(subject_name, module_id) VALUES (?,?)";
+    const typeSubjects = "INSERT INTO subjects(subject_name) VALUES (?)";
 
     const dataSubjects = [subject_name, module];
 
@@ -15,7 +15,7 @@ async function createSubjects(subject_name, module){
 
 async function deleteSubject(idSubject){
 
-    const sql = "UPDATE subjects SET soft_delete=1 WHERE subject_id = ?";
+    const sql = "UPDATE subjects SET soft_delete= 1 WHERE subject_id = ?";
 
     const conn = await database.connect();
     await conn.query(sql, [idSubject]);
@@ -23,7 +23,7 @@ async function deleteSubject(idSubject){
 }
 
 async function updateSubject(subject_id, subject_name, module) {
-    const sql = "UPDATE subjects SET subject_name = ?, module_id = ? WHERE subject_id = ?";
+    const sql = "UPDATE subjects SET subject_name = ? WHERE subject_id = ?";
     const dataSubjects = [subject_name, module, subject_id];
 
     const conn = await database.connect();
@@ -32,7 +32,7 @@ async function updateSubject(subject_id, subject_name, module) {
 }
 
 async function getSubject(subject_id) {
-    const sql = "SELECT * FROM subjects WHERE subject_id = ?";
+    const sql = "SELECT * FROM subjects WHERE subject_id = ? and soft_delete = 0";
     const conn = await database.connect();
     const [rows] = await conn.query(sql, [subject_id]);
     conn.end();

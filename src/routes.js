d8a9e026083ade153ec031d7cd460e0afaecd88b
Guilherme Alves <drdrakino@gmail.com>
2024-09-03 11:50:37 -0300

    feat: Implementar verificação de erro e rotas básicas

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import express from 'express';
 import subjectsController from './controllers/subjectsController.js'
 import userController from './controllers/userController.js'
 import coursesController from './controllers/coursesController.js';
+import localTypeController from './controllers/localTypeController.js'
 
 const routes = express();
 
 routes.use('/subjects', subjectsController);
 routes.use('/user', userController);
 routes.use('/courses', coursesController);
+routes.use('/local-type', localTypeController);
 
 export default routes;
--- src/services/localTypeService.js
+++ src/services/localTypeService.js
@@ -1,20 +1,22 @@
 import database from "../repository/mySQL.js"
 
-async function createType(){
+async function createType(type_name, description){
     const sql = "INSERT into location_types(type_name, description) VALUES(?, ?)";
 
-    const conn = await dabatase.connect();
-    const [rows] = await conn.query(sql);
-    conn.end();
-    return rows;
-}
-
-async function deleteType(idType){
-    const sql = "UPDATE location_types SET soft_delete= 1 WHERE location_id = ?";
+    const data = [type_name, description];
 
     const conn = await database.connect();
-    await conn.query(sql, idType);
+    await conn.query(sql, data);
     conn.end();
+}
+
+async function deleteType(id) {
+    const sql = "DELETE FROM location_types WHERE type_id = ?";
+
+    const conn = await database.connect();
+    const [rows] = await conn.query(sql, [id]);
+    conn.end();
+    return rows;
 }
 
 async function getAllTypes(){
@@ -26,22 +28,22 @@ async function getAllTypes(){
     return rows;
 }
 
-async function getOneType(idType){
-    const sql = "SELECT * from location_types WHERE id= ? and soft_delete = 0"
+async function getOneType(id){
+    const sql = "SELECT * from location_types WHERE type_id= ? and soft_delete = 0"
 
     const conn = await database.connect();
-    const [rows] = await conn.query(sql, idType);
+    const [rows] = await conn.query(sql, id);
     conn.end();
     return rows;
 }
 
-async function findSubjectByName(idType) {
+async function findTypeByName(id) {
     const sql = "SELECT * FROM location_types WHERE type_name = ? AND soft_delete = 0";
 
     const conn = await database.connect();
-    const [rows] = await conn.query(sql, idType);
+    const [rows] = await conn.query(sql, id);
     conn.end();
     return rows.length > 0 ? rows[0] : null;
 }
 
-export default {createType, deleteType, getAllTypes, getOneType, findSubjectByName};
+export default {createType, deleteType, getAllTypes, getOneType, findTypeByName};

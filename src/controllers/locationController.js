d586ab512665a6e0c32cbed310faf4e32c106e01
Bruno <142853863+brunofsilva2@users.noreply.github.com>
2024-09-11 21:04:16 -0300

    feat: criação do método post de "locations" com validação das chaves estrangeiras (#10)
    * feat: add create locations service and post method in controller with foreign key validation
    
    * fix: padronizando o código e removendo funções adicionais,

---
+++ src/controllers/locationController.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import locationService from '../services/locationService.js';
+
+const routes = express.Router();
+
+routes.post('/', async (req, res) => {
+    const { campus, building_id, floor_number, location_type_id, location_name, description } = req.body;
+
+    try {
+        if (!campus || !building_id || !floor_number || !location_type_id || !location_name || !description) {
+            return res.status(400).send({ success: false, message: 'Por favor, preencha todos os campos obrigatórios.' });
+        }
+
+        if (campus.length > 100) {
+            return res.status(400).send({ success: false, message: 'O nome do campus não pode ter mais que 100 caracteres.' });
+        }
+
+        if(location_name.length > 50){
+            return res.status(400).send({ success: false, message: 'O nome do local não pode conter mais que 50 caracteres'})
+        }
+
+        if(description.length > 200){
+            return res.status(400).send({ success: false, message: 'A descrição não pode conter mais que 200 caracteres.'});
+        }
+
+        const createLocationRes = await locationService.createLocation(campus, building_id, floor_number, location_type_id, location_name, description);
+
+        if (!createLocationRes.success) {
+            return res.status(400).send(createLocationRes);
+        }
+
+        return res.status(201).send(createLocationRes);
+
+    } catch (error) {
+        return res.status(500).send({ success: false, message: 'Erro interno do servidor.', error: error.message });
+    }
+});
+
+export default routes;
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import subjectsController from './controllers/subjectsController.js'
-import userController from './controllers/userController.js'
+import subjectsController from './controllers/subjectsController.js';
+import userController from './controllers/userController.js';
 import coursesController from './controllers/coursesController.js';
+import locationController from './controllers/locationController.js';
 import localTypeController from './controllers/localTypeController.js'
 
 const routes = express();
@@ -9,6 +10,7 @@ const routes = express();
 routes.use('/subjects', subjectsController);
 routes.use('/user', userController);
 routes.use('/courses', coursesController);
+routes.use('/locations', locationController);
 routes.use('/local-type', localTypeController);
 
 export default routes;

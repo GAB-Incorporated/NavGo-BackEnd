f3d47e8231bfd61ed35c08bde6350ab9ca54d827
Aurora Kruschewsky <124530477+AurorinhaBoreal@users.noreply.github.com>
2024-09-16 10:28:08 -0300

    refactor: Remove console.logs

--- src/services/validateAdminService.js
+++ src/services/validateAdminService.js
@@ -4,12 +4,9 @@ async function validateAdministrator(userId) {
     const sql = 'SELECT user_type FROM users WHERE user_id = ? AND soft_delete = 0';
 
     const conn = await database.connect();
-    console.log(`Conectado ao banco para validar o administrador. ID: ${userId}`); // Log para verificar se a conexão foi bem-sucedida.
 
     try {
         const [rows] = await conn.query(sql, [userId]);
-        console.log(`Resultado da consulta:`, rows); // Log do resultado da consulta
-
         if (rows.length === 0) {
             return {success: false, status: 404, message:'Usuário não encontrado.'};
         }
@@ -22,7 +19,6 @@ async function validateAdministrator(userId) {
 
         return { success: true };
     } catch (error) {
-        console.error(`Erro durante a validação do administrador: ${error.message}`);
         return { success: false, status: 500, message: 'Erro interno na validação do coordenador.'}
     } finally {
         conn.end();
